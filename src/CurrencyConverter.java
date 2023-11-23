a1631179be168daf3a80edd60d78a813a9f5c000
Kausik D <kausikdevanathan@gmail.com>
2023-11-23 18:50:32 +0530

    Not working Commit

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -6,13 +6,16 @@ import java.net.URL;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-
 public class CurrencyConverter {
-    private static final String API_KEY = "606c8a3a9877482db4536b4159b9df91";
+    private static final String API_KEY = "cf7facf348484aebb99656d13ae7f76b";
     private static final String API_URL = "https://open.er-api.com/v6/latest/";
 
     public static void main(String[] args) {
         try {
+            // Display available currencies
+            System.out.println("Available currencies: " + getAllCurrencies());
+
+            // Get user input
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             System.out.print("Enter the source currency code: ");
@@ -36,61 +39,8 @@ public class CurrencyConverter {
         }
     }
 
-    private static double getConversionRate(String sourceCurrency, String targetCurrency) {
-        try {
-            URL url = new URL(API_URL + sourceCurrency + "?app_id=" + API_KEY);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder response = new StringBuilder();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
-
-                // Parse JSON response to get conversion rate
-                String jsonResponse = response.toString();
-                double rate = parseJsonResponse(jsonResponse, targetCurrency);
-                if (rate != -1) {
-                    return rate;
-                } else {
-                    System.out.println("Invalid currency codes.");
-                    return -1;
-                }
-            } else {
-                System.out.println("Failed to fetch data from Open Exchange Rates API. Response Code: " + responseCode);
-                return -1;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return -1;
-        }
-    }
-
-    private static double parseJsonResponse(String jsonResponse, String targetCurrency) {
-        try {
-            // Parse JSON using Gson
-            JsonObject json = JsonParser.parseString(jsonResponse).getAsJsonObject();
-            JsonObject rates = json.getAsJsonObject("rates");
-
-            // Get the conversion rate for the target currency
-            if (rates.has(targetCurrency)) {
-                return rates.get(targetCurrency).getAsDouble();
-            } else {
-                System.out.println("Invalid target currency code.");
-                return -1;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
-    }
-    private static String getAllCurrencies(double conversionRate) {
+    // Method to fetch all available currencies
+    private static String getAllCurrencies() {
         try {
             URL url = new URL(API_URL + "?app_id=" + API_KEY);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -120,6 +70,45 @@ public class CurrencyConverter {
         } catch (IOException e) {
             e.printStackTrace();
             return "N/A";
+        }
+    }
+
+    // Method to fetch conversion rate
+    private static double getConversionRate(String sourceCurrency, String targetCurrency) {
+        try {
+            URL url = new URL(API_URL + sourceCurrency + "?app_id=" + API_KEY);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Parse JSON response to get conversion rate
+                String jsonResponse = response.toString();
+                JsonObject json = JsonParser.parseString(jsonResponse).getAsJsonObject();
+                JsonObject rates = json.getAsJsonObject("rates");
+
+                if (rates.has(targetCurrency)) {
+                    return rates.get(targetCurrency).getAsDouble();
+                } else {
+                    System.out.println("Invalid target currency code.");
+                    return -1;
+                }
+            } else {
+                System.out.println("Failed to fetch data from Open Exchange Rates API. Response Code: " + responseCode);
+                return -1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
         }
     }
 }
